e14e7fdf51b4ed2701b0d85b269f39cf40eaa01b
Pavel Strashnov <paul.engine@gmail.com>
2015-02-01 22:56:36 +0300

    Added core review comments

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 angular.module('toDoList', []).controller('toDoListController', function() {
+    //те объекты которые нужны на вьюхе должны класть в $scope
+    //this в JS это черная магия, его юзать надо только там где хорошо понимаешь зачем он:)
 	this.list = [];
-	
+
 	this.getRemainingCount = function() {
 	var left = 0;
 	angular.forEach(this.list,function(line) {
 		left += line.done ? 0 : 1;
 	});
-    return left;
+    return left;//за неправильную табуляцию программисты в аду горят
 	};
 
 	this.addNewTodo = function() {
