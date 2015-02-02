d821c4eaac42277f9b5d818e959f8461f808ba63
dihlophos <dihlophos2@gmail.com>
2015-02-02 19:00:45 +0300

    Mongo service added

---
+++ service.js
@@ -0,0 +1,33 @@
+myDBService = function ($http, $q) {
+
+    getItems = function (url) {
+
+        var deferred = $q.defer();
+
+        $http.get(url)
+            .success(function (data, status, headers, config) {
+                deferred.resolve(data);
+            }).error(function (data, status, headers, config) {
+                deferred.reject(data);
+            });
+
+        return deferred.promise;
+    };
+
+    upsertItem = function(url,item) {
+
+        var deferred = $q.defer();
+
+        $http.post(url, item)
+            .success(function (data, status, headers, config) {
+                deferred.resolve(data);
+            }).error(function (data, status, headers, config) {
+                deferred.reject(data);
+            });
+
+        return deferred.promise;
+    };
+
+    return { getItems: getItems, insertItem: insertItem };
+};
+
