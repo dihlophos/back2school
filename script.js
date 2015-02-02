c6f7eb257f3e818b53c9f3b43145f8cc93b91853
dihlophos <dihlophos2@gmail.com>
2015-02-02 18:08:57 +0300

    More proper css-classes names

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-angular.module('toDoList', []).controller('toDoListController', function() {
-    //те объекты которые нужны на вьюхе должны класть в $scope
-    //this в JS это черная магия, его юзать надо только там где хорошо понимаешь зачем он:)
-	this.list = [];
+angular.module('toDoList', []).controller('toDoListController', function($scope) {
+    
+	$scope.list = [];
 
-	this.getRemainingCount = function() {
-	var left = 0;
-	angular.forEach(this.list,function(line) {
-		left += line.done ? 0 : 1;
-	});
-    return left;//за неправильную табуляцию программисты в аду горят
+	$scope.getRemainingCount = function() {
+	    var left = 0;
+	    angular.forEach($scope.list, function (line) {
+		    left += line.done ? 0 : 1;
+	    });
+        return left;
 	};
 
-	this.addNewTodo = function() {
-		if (this.newLine) {
-		  this.list.push({text:this.newLine, done:false});
-		  this.newLine='';
+    $scope.addNewTodo = function () {
+	    if ($scope.newLine) {
+	        $scope.list.push({ text: $scope.newLine, done: false });
+	        $scope.newLine = '';
 		}
 	};
 });
