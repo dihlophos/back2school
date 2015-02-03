fdab82007835acfda8037b23e06591cba587b901
dihlophos <dihlophos2@gmail.com>
2015-02-03 09:18:21 +0300

    No more promises

--- script.js
+++ script.js
@@ -2,40 +2,40 @@ var app = angular.module('toDoList', []);
 app.controller('toDoListController', function ($scope, ToDoService) {
 
     ToDoService.queryToDoList()
-        .then(function (data) {
-            $scope.list = data;
-        }, function (data) {
-            console.log("Huh, that's bad...");
+        .then(function (answer) {
+            $scope.list = answer.data;
+        }, function (answer) {
+            console.log("Error. Status: ;" + answer.status + " StatusText: " + answer.statusText);
         });
-	
-	$scope.getRemainingCount = function() {
-	    var left = 0;
-	    angular.forEach($scope.list, function (line) {
-		    left += line.done ? 0 : 1;
-	    });
+
+    $scope.getRemainingCount = function () {
+        var left = 0;
+        angular.forEach($scope.list, function (line) {
+            left += line.done ? 0 : 1;
+        });
         return left;
-	};
+    };
 
     $scope.addNewTodo = function () {
-	    if ($scope.newLine) {
-			var todo = { _id: $scope.list.length, text: $scope.newLine, done: false }
-	        $scope.list.push(todo);
-			updateDBDocument(todo);
-	        $scope.newLine = '';
-		}
-	};
-	
-	$scope.updateTodo = function(line)	{
-		updateDBDocument(line);
-	}
-	
-	updateDBDocument = function (doc) {
-	    ToDoService.saveToDo(doc)
-			.then(function (data) {
-				console.log("Updated: id=" + doc._id);
-			}, function (data) {
-				console.log("Huh, that's bad...");
-			});	
-	}
-	
+        if ($scope.newLine) {
+            var todo = { _id: $scope.list.length, text: $scope.newLine, done: false }
+            $scope.list.push(todo);
+            updateDBDocument(todo);
+            $scope.newLine = '';
+        }
+    };
+
+    $scope.updateTodo = function (line) {
+        updateDBDocument(line);
+    }
+
+    updateDBDocument = function (doc) {
+        ToDoService.saveToDo(doc)
+			.then(function (answer) {
+			    console.log("Updated: id=" + doc._id);
+			}, function (answer) {
+			    console.log("Error. Status: ;" + answer.status + " StatusText: " + answer.statusText);
+			});
+    }
+
 });
