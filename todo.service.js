038fb42b1ec51fd48e599f0a2f241f99a24e8ab4
dihlophos <dihlophos2@gmail.com>
2015-02-03 09:18:47 +0300

    No more promises

--- todo.service.js
+++ todo.service.js
@@ -1,35 +1,14 @@
-var app = angular.module('toDoList').factory('ToDoService', function ($http, $q) {
+var app = angular.module('toDoList').factory('ToDoService', function ($http) {
 
     var todoCollectionUrl = "https://api.mongolab.com/api/1/databases/tododb/collections/todolist?apiKey=CRZEn0TYiLT1CZpV1_1Jr5LfUvEp4O3n";
 
     queryToDoList = function () {
-
-        var deferred = $q.defer();
-
-        //$http.xxx и так возвращает промис.
-        //вряд ли есть смысл делать свой собственный промис
-        $http.get(todoCollectionUrl)
-            .success(function (data, status, headers, config) {
-                deferred.resolve(data);
-            }).error(function (data, status, headers, config) {
-                deferred.reject(data);
-            });
-
-        return deferred.promise;
+        return $http.get(todoCollectionUrl);
     };
 
     saveToDo = function (todo) {
 
-        var deferred = $q.defer();
-
-        $http.post(todoCollectionUrl, todo)
-            .success(function (data, status, headers, config) {
-                deferred.resolve(data);
-            }).error(function (data, status, headers, config) {
-                deferred.reject(data);
-            });
-
-        return deferred.promise;
+        return $http.post(todoCollectionUrl, todo);
     };
 
     return { queryToDoList: queryToDoList, saveToDo: saveToDo };
